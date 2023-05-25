4901534dcb6f70d4347930a3e3cf4778b1bfbb2c
Denperidge <denperidge@gmail.com>
2023-05-25 20:35:16 +0200

    First draft of random shapes

--- src/index.js
+++
@@ -1,9 +0,0 @@
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-document.getElementById("background").style.left = "100vw";
-document.getElementById("background").style.transform = "rotate(500deg)";
-
-
-//$("#background").style("left", "100vw");
