262334fdbe348cc8eb3038d7b6e0f58ce3b9e8c9
Denperidge <denperidge@gmail.com>
2023-07-21 21:31:11 +0200

    Refactors

--- .pugrc.js
+++ .pugrc.js
@@ -1,14 +1,15 @@
 // Import wasn't supported
 const fs = require("fs");
-const srcDir = "src/";
+const path = require("path");
 
 function collectFiles(dir, read=false, includePath=true) {
     const array = []
     fs.readdirSync(dir).forEach((filename) => {
         const filepath = dir + filename;
+        const relativePath = path.relative("src/", filepath).replaceAll("\\", "/");
 
         if (!read) {
-            if (includePath) array.push(filepath.replace(srcDir, ""));
+            if (includePath) array.push(relativePath);
             else array.push(filename);
         } else {
             array.push(fs.readFileSync(filepath, {encoding: "utf-8"}));
@@ -17,9 +18,9 @@ function collectFiles(dir, read=false, includePath=true) {
     return array;
 }
 
-const navs = collectFiles(srcDir + "nav/", read=true);
-const plants = collectFiles(srcDir + "plants/img/", read=false);
-const songs = collectFiles(srcDir + "music/songs/", read=false, includePath=false);
+const navs = collectFiles("nav/", read=true);
+const plants = collectFiles("images/plants/", read=false);
+const songs = collectFiles("songs/", read=false, includePath=false);
 
 
 module.exports = {
