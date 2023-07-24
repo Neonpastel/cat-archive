661e003ab4b6f31d39c37fa792cf2e68f59157eb
Denperidge <denperidge@gmail.com>
2023-07-24 16:55:14 +0200

    Slightly styled intro, plants now use screen width

--- src/4-garden/plants.js
+++ src/4-garden/plants.js
@@ -1,8 +1,10 @@
 function plantPlants() {
     let plants = $(".plant");
+    let { availWidth, availHeight } = screen;
+
     for (let i = 0; i < plants.length; i++) {
         $(plants[i])
-            .css("left", random(-10,110) + "%")
+            .css("left", random(-10,availWidth + 10) + "px")
             .css("bottom", "-" + random(10, 15) + "px")
             .css("z-index", random(0, 15));
     }
